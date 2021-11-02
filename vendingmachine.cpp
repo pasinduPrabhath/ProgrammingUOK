ffd3b696439c7e55b937faa3cd2f5f9c328aaf38
Pasindu Prabhath <56954364+pasinduPrabhath@users.noreply.github.com>
2021-11-02 21:54:06 +0530

    Add files via upload

--- vendingmachine.cpp
+++ vendingmachine.cpp
@@ -1,74 +1,28 @@
-//IM/2019/069 - Pasindu Prabhath 
+//IM/2019/088 
+//Manahara Bibulewela
+
 #include <iostream>
 using namespace std;
-
-int productslist(int items[4][2]);
-int calculations(int total, int coins[4], int balance, int cost);
-int getcoins(int coins[4]);// 0 means Rs 10 coins
-int cashout(int balance);
 
 int coincount[4] = {100,100,100,100};
 int tempbalance;
 
-int main()
-{
-    int items[4][2] = {{110,30},{120,50},{150,60},{200,70}};
-    int coins[4];
-  
-    int inputnum,total,balance,cost,continuining = 0;
-    int pw = 123;//password
-    
-    while(continuining == 0)
-    {
-        productslist(items);
-
-        cout<<"Enter the number of wanted product : "; //getting the product number
-        cin>>inputnum;
-
-        for(int i = 0; inputnum > 4 || inputnum < 1; i++)
-        {
-            cout<<"Enter a number in range in the list : ";
-            cin>>inputnum;
-        }
-
-        cout<<"Selected product is product "<<inputnum<<" , Enter Rs. "<< items[inputnum-1][0]<<" to purchase.\n";
-
-        getcoins(coins);
-
-        cost = items[inputnum - 1][0];
-        calculations(total, coins, balance, cost);
-
-        cout<<"do you want to do another transaction? If yes, then press 0, or if you are developer, then insert password to abort \n your input : ";
-        cin>>continuining;
-        while(continuining != 0 && continuining != pw)
-        {
-            cout<<"Please input correct number : ";
-            cin>>continuining;
-        }
-        if(continuining == pw)
-        {
-            cout<<"Aborted!";
-        }
-    }
-    return 0;
-}
-
-int productslist(int items[4][2]) //prints products list
+int productslist(int items[4][2]) //products list
 {
     for(int i = 0; i<4; i++)
     {
         cout << i+1 << ") Price of product "<< i+1 <<" : " << items[i][0] << "  Available Quantity : " << items[i][1] << "\n";
     }
-    cout<<"----------------------------------------------------\n"; //border
+    cout<<"----------------------------------------------------\n"; 
 }
 
-int getcoins(int coins[4]) // get coin inputs
+int getcoins(int coins[4]) //coin inputs
 {
     string money[] = {"Rs 10 coins", "Rs 5 coins", "Rs 2 coins", "Rs 1 coins"};
     for (int i = 0; i < 4; i++)
     {
         cout<<"Please enter amount of "<<money[i]<<" : ";
-        cin>>coins[i]; // 0 = Rs 10
+        cin>>coins[i]; // [0] = Rs 10
         coincount[i] += coins[i];
 
         if(coincount[i] > 120)
@@ -78,27 +32,6 @@ int getcoins(int coins[4]) // get coin inputs
             coincount[i] = 120;//removing excess coins
         }
     }
-}
-
-int calculations(int total, int coins[4], int balance, int cost) // calculations
-{
-    total = coins[0] * 10 + coins[1] * 5 + coins[2] * 2 + coins[3];
-    balance = total - cost;
-    int newcoins;
-    
-    for(int j = 0; balance < 0; j++)
-    {
-        
-        tempbalance = total - cost;
-        cout<<"Insuffiecient balance, Please enter more Rs."<<-1 * tempbalance<<"\n";
-        // total = 0;
-        getcoins(coins);
-        total = coins[0] * 10 + coins[1] * 5 + coins[2] * 2 + coins[3];
-        balance = total + tempbalance;
-    }
-   
-    cout<<"Your balance is : "<<balance<<endl;
-    cashout(balance);
 }
 
 int cashout(int balance)
@@ -156,3 +89,68 @@ int cashout(int balance)
         cout<<"Out of change "<<endl;
     }
 }
+
+int calculations(int total, int coins[4], int balance, int cost)
+{
+    total = coins[0] * 10 + coins[1] * 5 + coins[2] * 2 + coins[3];
+    balance = total - cost;
+    int newcoins;
+    
+    for(int j = 0; balance < 0; j++)
+    {
+        
+        tempbalance = total - cost;
+        cout<<"Insuffiecient balance, Please enter more Rs."<<-1 * tempbalance<<"\n";
+        getcoins(coins);
+        total = coins[0] * 10 + coins[1] * 5 + coins[2] * 2 + coins[3];
+        balance = total + tempbalance;
+    }
+   
+    cout<<"Your balance is : "<<balance<<endl;
+    cashout(balance);
+}
+
+
+int main()
+{
+    int items[4][2] = {{110,30},{120,50},{150,60},{200,70}};
+    int coins[4];
+  
+    int inputnum,total,balance,cost,continuining = 0;
+    int pw = 321;//password
+    
+    while(continuining == 0)
+    {
+        productslist(items);
+
+        cout<<"Enter the number of wanted product : "; //product number
+        cin>>inputnum;
+
+        for(int i = 0; inputnum > 4 || inputnum < 1; i++)
+        {
+            cout<<"Enter a number in range in the list : ";
+            cin>>inputnum;
+        }
+
+        cout<<"Selected product is product "<<inputnum<<" , Enter Rs. "<< items[inputnum-1][0]<<" to purchase.\n";
+
+        getcoins(coins);
+
+        cost = items[inputnum - 1][0];
+        calculations(total, coins, balance, cost);
+
+        cout<<"do you want to do another transaction? If yes, then press 0, or if you are developer, then insert password to abort \n your input : ";
+        cin>>continuining;
+        while(continuining != 0 && continuining != pw)
+        {
+            cout<<"Please input correct number : ";
+            cin>>continuining;
+        }
+        if(continuining == pw)
+        {
+            cout<<"Aborted!";
+        }
+    }
+    
+}
+
